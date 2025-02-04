e0f7f0e5e75b8b15f150ad1546df679b284d9c2e
yuichi.oba <yuichi.oba@sorich.jp>
2025-02-04 22:41:00 +0900

    add pwa setting

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,19 @@ import App from './App.vue'
 import router from './router'
 import './index.css'
 
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("新しいバージョンが利用可能です。更新しますか？")) {
+      updateSW(true)
+    }
+  },
+  onOfflineReady() {
+    alert("アプリはオフラインでも使用できます！");
+  },
+});
+
 const app = createApp(App)
 
 app.use(router)

eba164f4b2f5e7ecf2fb6294e43a764f28c2396d
yuichi.oba <yuichi.oba@sorich.jp>
2025-02-22 13:05:33 +0900

    シェア画面の実装

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { WelcomePage } from '@/pages/welcome'
 import { KaimemoPage } from '@/pages/kaimemo'
 import { KaimemoSummaryPage } from '@/pages/kaimemo-summary'
+import { SharePage } from '@/pages/share'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'share',
+      component: SharePage,
+    },
+    {
+      path: '/kaimemo',
       name: 'kaimemo',
       component: KaimemoPage,
     },

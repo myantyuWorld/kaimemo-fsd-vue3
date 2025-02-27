7b801b52d59757513bfd2da7d7f892d5962a4d31
yuichi.oba <yuichi.oba@sorich.jp>
2025-02-27 21:43:57 +0900

    集計画面の状態を保存して、カレンダー画面で処理するよう実装

--- src/pages/summary-calender/ui/SummaryCalender.vue
+++ src/pages/summary-calender/ui/SummaryCalender.vue
@@ -6,36 +6,19 @@ const props = defineProps<{
   date: string
 }>()
 
-const attr = [
-  {
-    key: 'today',
-    highlight: 'red',
-    dates: [new Date()],
-    event: {
-      label: 'test',
-    },
-    popover: {
-      label: 'Today',
-    },
-  },
-]
-
-useInteraction({ id: () => props.date })
+const { calenderAttributes } = useInteraction({ id: () => props.date })
 </script>
 
 <template>
-  <Calendar :date="props.date" expanded :attributes="attr" />
+  <Calendar :date="props.date" expanded :attributes="calenderAttributes" />
 </template>
 
 <style>
 .vc-title {
   font-size: 1.5rem;
 }
-.vc-day {
-  height: 100px;
-}
 
 .vc-day-content {
-  height: 100px;
+  height: 80px;
 }
 </style>
